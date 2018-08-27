e0e7e82afd1f11d53c21b725435b58593f9ae79c
Jeremy Bost <jeremybost@outlook.com>
2018-08-26 23:52:09 -0400

    Slightly better complete animation

--- prodd/lib/screens/goals/goal_screen.dart
+++ prodd/lib/screens/goals/goal_screen.dart
@@ -40,42 +40,58 @@ class GoalList extends StatefulWidget {
 
 class _GoalListState extends State<GoalList> {
 
+  List<Goal> goals = List(), removedGoals = List();
   final GlobalKey<AnimatedListState> _listKey = GlobalKey<AnimatedListState>();
-  List<Goal> goals = List();
+  bool _loading = true;
 
   @override
   void initState() {
     super.initState();
 
     widget.goalRepo.activeGoalChangesStream().listen((List<Change<Goal>> changes) {
-      changes.forEach((change) {
-        switch (change.type) {
-          case ChangeType.added:
-            goals.insert(change.newIndex, change.data);
-            _listKey.currentState.insertItem(change.newIndex);
-            break;
-          case ChangeType.modified:
-            goals.removeAt(change.oldIndex);
-            goals.insert(change.newIndex, change.data);
-            break;
-          case ChangeType.removed:
-            goals.removeAt(change.oldIndex);
-            _listKey.currentState.removeItem(change.oldIndex, (context, animation) {
-              return Text("bye");
-            });
-            break;
-        }
-      });
+      if(_loading) {
+        goals = changes.map((c) => c.data).toList();
+        _loading = false;
+      }
+      else {
+        changes.forEach((change) {
+          switch (change.type) {
+            case ChangeType.added:
+              goals.insert(change.newIndex, change.data);
+              _listKey.currentState.insertItem(change.newIndex);
+              break;
+            case ChangeType.modified:
+              goals.removeAt(change.oldIndex);
+              goals.insert(change.newIndex, change.data);
+              break;
+            case ChangeType.removed:
+              goals.removeAt(change.oldIndex);
+              removedGoals.add(change.data);
+              final i = removedGoals.length - 1;
+              _listKey.currentState.removeItem(change.oldIndex, (context, animation) {
+                return ScaleTransition(
+                  scale: animation,
+                  child: _GoalItem(goal: removedGoals[i], removing: true)
+                );
+              }, duration: Duration(milliseconds: 200));
+              break;
+          }
+        });
+      }
+
       setState(() {
-        
       });
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    if(_loading) {
+      return Text("Loading");
+    }
     return AnimatedList(
       key: _listKey,
+      initialItemCount: goals.length,
       itemBuilder: (context, i, animation) {
         return _GoalItem(goal: goals[i], goalRepo: widget.goalRepo);
       },
@@ -84,10 +100,17 @@ class _GoalListState extends State<GoalList> {
 }
 
 class _GoalItem extends StatelessWidget {
-  _GoalItem({this.goal, this.goalRepo});
+  _GoalItem({@required this.goal, this.goalRepo, this.removing = false})
+    : assert(goal != null);
 
   final Goal goal;
   final GoalRepository goalRepo;
+  final bool removing;
+
+  void _onComplete() {
+    goal.status = GoalStatus.completed;
+    goalRepo?.saveGoal(goal);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -95,17 +118,16 @@ class _GoalItem extends StatelessWidget {
       title: Text(goal.title),
       subtitle: goal.completeBy != null ? Text(DateFormat().add_yMEd().add_jm().format(goal.completeBy)) : null,
       leading: IconButton(
-        icon: Icon(Icons.check_box_outline_blank),
-        onPressed: () {
-          goal.status = GoalStatus.completed;
-          goalRepo.saveGoal(goal);
-        },
+        icon: Icon(removing ? Icons.check_box : Icons.check_box_outline_blank),
+        onPressed: _onComplete,
       ),
       //trailing: Text(goal.status.toString()),
-      onTap: () => Navigator.of(context).push(MaterialPageRoute(
-        builder: (context) => AddEditGoalScreen(goal: goal, goalRepo: goalRepo,)
-      )),
+      onTap: () {
+        if(removing) return;
+        Navigator.of(context).push(MaterialPageRoute(
+          builder: (context) => AddEditGoalScreen(goal: goal, goalRepo: goalRepo,)
+        ));
+      },
     );
   }
-
 }
